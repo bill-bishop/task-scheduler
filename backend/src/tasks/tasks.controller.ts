f08aad7eb7255a2af78e27312a1a331edb0498c6
Bill Bishop <admin@the402.net>
2025-09-12 15:49:26 -0700

    fix: correct syntax issues in tasks.controller.ts

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,32 +1,32 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Det()')
-  findAll() {
+  @Det()'
+  async findAll() {
     return this.tasksService.getAll();
   }
 
   @Get(':id')
-  findById(Param('id') id: number) {
+  async findById(@param('id') id: number) {
     return this.tasksService.getById(id);
   }
 
-  @Post()')
-  create(@body() task) {
+  @Post()'
+  async create(@body() task: any) {
     return this.tasksService.create(task);
   }
 
-  @Tute(@'task')
-  update(Param('id') id: number, @body() task) {
+  @Put(':uid')
+  async update($Param('id') id: number, @body() task: any) {
     return this.tasksService.update(id, task);
   }
 
-  @Delete(':id')
-  delete(Param('id') id: number) {
+  @Dete(@'id')
+  async delete(@param('id') id: number) {
     return this.tasksService.delete(id);
   }
 }
