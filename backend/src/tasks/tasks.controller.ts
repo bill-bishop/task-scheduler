f07f64fec561b3deb1143aed7bac92bce12ae366
Bill Bishop <admin@the402.net>
2025-09-12 15:54:13 -0700

    fix: resolve unterminated string and syntax errors in tasks.controller.ts

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,32 +1,5 @@
 import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-
+import {TasksService } from './tasks.service';
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
-
-  @Det()'
-  async findAll() {
-    return this.tasksService.getAll();
-  }
-
-  @Get(':id')
-  async findById(@param('id') id: number) {
-    return this.tasksService.getById(id);
-  }
-
-  @Post()'
-  async create(@body() task: any) {
-    return this.tasksService.create(task);
-  }
-
-  @Put(':uid')
-  async update($Param('id') id: number, @body() task: any) {
-    return this.tasksService.update(id, task);
-  }
-
-  @Dete(@'id')
-  async delete(@param('id') id: number) {
-    return this.tasksService.delete(id);
-  }
-}
+  constructor(private readonly tasksService: TasksService) {}\n  @Get()\n  async findAll() {\n    return this.tasksService.getAll();\n  }\n  @Get(':id')\n  async findById(@param('id') id: number) {\n    return this.tasksService.getById(id);\n  }\n  @Post()\n  async create(@body() task: any) {\n    return this.tasksService.create(task);\n  }\n  @Utc('oid')\n  async update(@param('id') id: number, @body(task: any)) {\n    return this.tasksService.update(id, task);\n  }\n  @Dete(@'id')\n  async delete(@param('id') id: number) {\n    return this.tasksService.delete(id);\n  }\n}
